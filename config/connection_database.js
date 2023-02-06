3cee3e3717bcdfcf2ce7ab17482338139fd9e078
nhannguyen24 <dnhan2426@gmail.com>
2023-02-06 10:40:48 +0700

    Fix config

--- config/connection_database.js
+++ config/connection_database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('swddatabase', 'root', 'quan', {
-  host: 'localhost',
+const sequelize = new Sequelize('uuacyt2ejjgpgzw4', 'us4zqsaqbcsdmgwb', 'obkcazvwd8guv7ia', {
+  host: 'g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   dialect: 'mysql',
   logging: false,
 });
