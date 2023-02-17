76d5487c386a66495f6aae576f65ee5f899c8bdc
phuclh1406 <phuclh1406@gmail.com>
2023-02-17 16:14:21 +0700

    uploadfile

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const {getAllPost, getPostById} = require('./post');
 const loginGoogle = require('./auth');
 const getAllStudent = require('./student');
+const uploadFile = require('./uploadFile');
 
 
-module.exports = {getAllPost, getPostById, loginGoogle, getAllStudent};
+module.exports = {getAllPost, getPostById, loginGoogle, getAllStudent, uploadFile};
