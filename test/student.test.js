0e74fb551d7c48a1e1156ef109d5ce827a9af13f
phuclh1406 <phuclh1406@gmail.com>
2023-03-09 17:11:44 +0700

    studentManage unit test

--- test/student.test.js
+++ test/student.test.js
@@ -1,7 +1,7 @@
 const services = require("../services");
 const { BadRequestError, InternalServerError } = require("../errors");
 const joi = require("joi");
-const { getAllStudent, deleteStudent } = require("../controllers/student");
+const { getAllStudent, deleteStudent, updateStudent, getAllStudentPaging, updateProfile } = require("../controllers/student");
 
 //GET method
 describe("getAllStudent function", () => {
@@ -125,3 +125,95 @@ it("should throw an error if delete student function returns an error", async ()
     expect(error).toEqual(expectedError);
   }
 });
+
+
+// getAllStudentPaging
+describe('getAllStudentPaging function', () => {
+  it('should return a 200 response with the correct data', async () => {
+    // Arrange  
+    const req = { query: {}, user: { role_name: 'admin' } };
+    const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+    const expectedResponse = { /* Add the expected response object here */ };
+
+    // Mock the services.getAllStudentPaging function to return the expected response
+    services.getAllStudentPaging = jest.fn().mockResolvedValue(expectedResponse);
+
+    // Act
+    await getAllStudentPaging(req, res);
+
+    // Assert
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(expectedResponse);
+  });
+});
+
+describe('updateStudent function', () => {
+  it('should return a 200 response with the updated student data', async () => {
+    // Arrange
+    const req = {
+      body: {
+        student_id: 'V2sSC1HSLASNtTT0RhzwqDxxwri2',
+        // other fields to update
+      }
+    };
+    const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+    const expectedResponse = { /* Add the expected response object here */ };
+
+    // Mock the services.updateStudent function to return the expected response
+    services.updateStudent = jest.fn().mockResolvedValue(expectedResponse);
+  
+    // Act
+    await updateStudent(req, res);
+
+    // Assert
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(expectedResponse);
+  });
+
+})
+
+describe('updateProfile', () => {
+  it('should return status 200 with updated profile information', async () => {
+    const mockReq = {
+      body: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: 'password123',
+      },
+      user: { student_id: 1 },
+    };
+
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const mockUpdateProfile = jest.spyOn(services, 'updateProfile');
+    mockUpdateProfile.mockResolvedValueOnce({
+      message: 'Profile updated successfully',
+      data: {
+        id: 1,
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+      },
+    });
+
+    await updateProfile(mockReq, mockRes);
+
+    expect(mockUpdateProfile).toHaveBeenCalledWith(
+      mockReq.body,
+      mockReq.user.student_id
+    );
+
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockRes.json).toHaveBeenCalledWith({
+      message: 'Profile updated successfully',
+      data: {
+        id: 1,
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+      },
+    });
+  });
+
+})
